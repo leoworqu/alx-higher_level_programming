cd77949dd8fc96f0f3970eb978879847780954a7
leoworqu <leoworqu@gmail.com>
2023-11-06 16:34:42 -0800

    1c

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Module 1-my_list
+
+Contains class MyList
+inherits from list; has public instance method to print sorted
+"""
+
 class MyList(list):
     """ my parent child"""
     def print_sorted(self):
