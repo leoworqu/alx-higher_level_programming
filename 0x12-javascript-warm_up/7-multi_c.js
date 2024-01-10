f56f0212261e710f1f80b0c36acd71da8e9fddb9
leoworqu <leoworqu@gmail.com>
2024-01-10 04:29:51 -0800

    8

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,11 +3,10 @@
 // Script that prints three lines using an array of strings and a loop
 const x = process.argv[2];
 
-if (!isNaN(x)){
+if (!isNaN(x)) {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
-}
-else {
+} else {
   console.log('Missing number of occurrences');
-  }
+}
