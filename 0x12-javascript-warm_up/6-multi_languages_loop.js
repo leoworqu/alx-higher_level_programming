634d37215467f24be9251d6416a93898c8e6a144
leoworqu <leoworqu@gmail.com>
2024-01-10 03:58:18 -0800

    6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Script that prints three lines using an array of strings and a loop
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
