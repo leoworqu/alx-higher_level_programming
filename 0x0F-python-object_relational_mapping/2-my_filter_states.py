89c4e06bc48a1b2965bdf3ea7fee8b6edaa6b3f0
leoworqu <leoworqu@gmail.com>
2024-01-13 23:28:25 -0800

    22

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,6 +8,9 @@ states table of hbtn_0e_0_usa where name matches the argument.
 """
 
 def search_states_by_name(username, password, database, state_name):
+    """ a script that takes in an argument and displays all values in the
+    states table of hbtn_0e_0_usa where name matches the argument.
+    """
     conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
 
@@ -25,4 +28,3 @@ def search_states_by_name(username, password, database, state_name):
 
 if __name__ == "__main__":
     search_states_by_name(*sys.argv[1:])
-
