95d15e3184a97af45524d6c813df385f83abbe0d
Leoworqu <leoworqu@gmail.com>
2024-02-20 23:44:11 -0800

    Lal

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// a script that gets the contents of a webpage and stores it in a file.
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode !== 200) {
+    console.error(response.statusCode);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
