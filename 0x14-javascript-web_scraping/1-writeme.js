ecb115ac1c12ddc11bbda9bdfe36e166b88773e0
Leoworqu <leoworqu@gmail.com>
2024-02-20 23:10:18 -0800

    moree

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+//  a script that writes a string to a file.
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Content has been written to ${filePath}`);
+  }
+});
