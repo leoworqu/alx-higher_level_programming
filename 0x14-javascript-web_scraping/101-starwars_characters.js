ca7e9424846e1a4ed697101e25f42c3a80f8695f
Leoworqu <leoworqu@gmail.com>
2024-02-21 00:44:19 -0800

    fix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,10 +1,26 @@
 #!/usr/bin/node
-//  a script that prints all characters of a Star Wars movie:
+// a script that prints all characters of a Star Wars movie:
 
 const request = require('request');
 
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+// Function to fetch character details from a character URL
+function fetchCharacter(characterUrl) {
+  return new Promise((resolve, reject) => {
+    request.get(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode !== 200) {
+        reject(new Error(`${response.statusCode}`));
+      } else {
+        const character = JSON.parse(body);
+        resolve(character.name);
+      }
+    });
+  });
+}
 
 request.get(apiUrl, (error, response, body) => {
   if (error) {
@@ -13,20 +29,14 @@ request.get(apiUrl, (error, response, body) => {
     console.error(response.statusCode);
   } else {
     const movie = JSON.parse(body);
-    const characters = movie.characters;
-
-    // Fetch character details for each character URL
-    characters.forEach(characterUrl => {
-      request.get(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else if (response.statusCode !== 200) {
-          console.error(response.statusCode);
-        } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
+    Promise.all(movie.characters.map(fetchCharacter))
+      .then(characterNames => {
+        characterNames.forEach(name => {
+          console.log(name);
+        });
+      })
+      .catch(err => {
+        console.error(err);
       });
-    });
   }
 });
