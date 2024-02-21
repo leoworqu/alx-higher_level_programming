bf36771c2f278a26d63226b619f5a35a9a9211c1
Leoworqu <leoworqu@gmail.com>
2024-02-20 23:04:54 -0800

    #!/usr/bin/nod

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
 //  a script that reads and prints the content of a file.
-
 const fs = require('fs');
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  console.log(err || content);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
