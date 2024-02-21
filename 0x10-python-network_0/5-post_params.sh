9885867ba7a75d248c9d7b2f9a53fcdcf7eae2b3
leoworqu <leoworqu@gmail.com>
2024-02-21 03:41:13 -0800

    $1

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#a Bash script that takes in a URL, sends a POST request to the passed URL
+ curl -s -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
