c76a6c8d690c192740364b802439fd7bf75d025e
leoworqu <leoworqu@gmail.com>
2024-02-21 03:55:29 -0800

    $1

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-"""This module contains functions to 
-find peaks in a list of unsorted ints."""
+"""find peaks in a list of unsorted ints."""
 
 def find_peak(list_of_integers):
-     """Find a peak in a list of unsorted integers.
-
-    Args:
-        list_of_integers (list): A list of unsorted integers.
-
-    Returns:
-        int: A peak in the list of integers.
-    """
+    """Find a peak in a list of unsorted integers."""
     if not list_of_integers:
         return None
 
