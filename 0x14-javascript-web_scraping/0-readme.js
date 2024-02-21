5482acd4f59db1f9bd86d89a81c1de78d3fe5f29
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-02-21 04:22:30 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -7,10 +7,8 @@ const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
-    // Print the error object if an error occurred
     console.error(err);
   } else {
-    // Print the content of the file
     console.log(data);
   }
 });
