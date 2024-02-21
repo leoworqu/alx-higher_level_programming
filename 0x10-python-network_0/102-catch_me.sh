f4abf14db346191ec2e506fa1b53acbedbfab405
leoworqu <leoworqu@gmail.com>
2024-02-21 04:08:52 -0800

    $1

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a POST request to 0.0.0.0:5000/catch_me with a custom user agent header
-curl -s -X PUT -H "User-Agent: I am a secret agent" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
