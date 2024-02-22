049aaeaa9b024507e952f368b9a0dbd735f508b2
leoworqu <leoworqu@gmail.com>
2024-02-22 03:04:47 -0800

    sadd

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,10 +8,7 @@ the header of the response.
 
 import urllib.request
 import sys
-
 url = sys.argv[1]
-
 with urllib.request.urlopen(url) as response:
     x_request_id = response.getheader('X-Request-Id')
-
 print(x_request_id)
