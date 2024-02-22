7315d29f5a07b55ce67243615648334a66465bb2
leoworqu <leoworqu@gmail.com>
2024-02-22 04:06:38 -0800

    doneeeeeee

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     repository_name = sys.argv[1]
     owner_name = sys.argv[2]
 
-    url = f'https://api.github.com/repos/{owner_name}/{repository_name}/commits'
+    u = f'https://api.github.com/repos/{owner_name}/{repository_name}/commits'
     params = {'per_page': 10}
-    response = requests.get(url, params=params)
+    response = requests.get(u, params=params)
 
     if response.status_code == 200:
         commits = response.json()
@@ -23,4 +23,4 @@ if __name__ == "__main__":
             author_name = commit['commit']['author']['name']
             print(f"{sha}: {author_name}")
     else:
-        print(f"Failed to retrieve commits. Status code: {response.status_code}")
+        print(f"Status code: {response.status_code}")
