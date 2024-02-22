71a94a241d4d61fac30a519260b74a262027663f
leoworqu <leoworqu@gmail.com>
2024-02-22 03:15:37 -0800

    error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8).
+"""
+
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            decoded_html = html.decode('utf-8')
+            print(decoded_html)
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
