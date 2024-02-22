b0650b14f375ca0080ce89232faba26bb995e052
leoworqu <leoworqu@gmail.com>
2024-02-22 03:22:59 -0800

    header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+akes in a URL, sends a request to the URL and
+displays the value of the variable X-Request-Id
+"""
+
+
+import requests
+import sys
+
+url = sys.argv[1]
+response = requests.get(url)
+
+x_request_id = response.headers.get('X-Request-Id')
+print(x_request_id)
