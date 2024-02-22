17a94e1ed98c784be70fdc8a7299cda7bf922343
leoworqu <leoworqu@gmail.com>
2024-02-22 03:32:25 -0800

    erroremail

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays body of the response.
+"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    status_code = response.status_code
+
+    if status_code >= 400:
+        print("Error code:", status_code)
+    else:
+        print(response.text)
