c0cac300cf1dad606c7b92fa0684739fcea5a074
leoworqu <leoworqu@gmail.com>
2024-02-22 03:26:06 -0800

    guess

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-akes in a URL, sends a request to the URL and
-displays the value of the variable X-Request-Id
+takes a URL, sends a request and displays value of the variable X-Request-Id
 """
 
 
